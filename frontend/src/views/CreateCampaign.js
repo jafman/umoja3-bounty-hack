39b514821cd79615b3539b4b62100cc6e4358a94
Jafar Kamarudeen <kamsy74@gmail.com>
2022-05-01 09:24:44 +0100

    clean code

--- frontend/src/views/CreateCampaign.js
+++ frontend/src/views/CreateCampaign.js
@@ -5,17 +5,17 @@ function CreateCampaign(){
       <hr/>
       <form className="campaign_form">
         <div className="mb-3">
-          <label for="camp_title" className="form-label">Campaign Title</label>
+          <label htmlFor="camp_title" className="form-label">Campaign Title</label>
           <input type="email" className="form-control" id="camp_title" />
         </div>
 
         <div className="mb-3">
-          <label for="description" className="form-label">Description</label>
+          <label htmlFor="description" className="form-label">Description</label>
         <textarea style={{height: "100px"}} className="form-control" id="description" />
         </div>
 
         <div className="mb-3">
-          <label for="amount" className="form-label">Amount</label>
+          <label htmlFor="amount" className="form-label">Amount</label>
           <input type="number" className="form-control" id="amount" />
           <div id="amountHelp" className="form-text">Amount in ALGO.</div>
         </div>
