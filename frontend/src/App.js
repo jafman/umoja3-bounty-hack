e154a47b1df56e24d0db91d5c5be4b1985806a3d
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-26 10:16:59 +0100

    updated app.js and Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from './views/Home'
 import CampaignWrapper from './views/CampaignWrapper';
 import CampaignDetails from './views/CampaignDetails';
 import CreateCampaign from './views/CreateCampaign';
+import Login from './views/Login';
 import {
   BrowserRouter,
   Routes,
@@ -14,6 +15,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
         <Route path="/campaign" element={<CampaignWrapper/>}>
           <Route path=":campaignId" element={<CampaignDetails />} />
           <Route path="new" element={<CreateCampaign />} />
