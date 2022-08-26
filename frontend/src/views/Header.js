cdf18d0e722258e38f5fb58809cda9c7c9608c82
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-26 10:16:31 +0100

    updated header

--- frontend/src/views/Header.js
+++ frontend/src/views/Header.js
@@ -1,18 +1,28 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
 
-function Header() {
+function Header(props) {
+  let navClass = 'navbar' ;
+  
+
   return (
-      <nav className="navbar shadow">
-        <div className="container-fluid">
+      <nav className={navClass}>
+        <div className="container">
           <Link to="/" id="logo" className="navbar-brand">
-            W3FUND
+            <img src={logo} alt={'logo'} width={120} />
           </Link>
+        {
+          props.showLinks === true && 
+
           <ul>
-            <li>How it works</li>
+            <li><Link to="/">How it works</Link></li>
+            <li><Link to="/">Logout</Link></li>
             <li>
-              <Link to="/campaign/new"><button type="button" className="btn btn-success">Start a Campaign</button></Link>
+              <Link to="/campaign/new"><button type="button" className="btn btn-success">Auction an item</button></Link>
             </li>
           </ul>
+
+        }
         </div>
       </nav>
       
