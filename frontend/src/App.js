4cf6802409758693f06eb6333cef042c57188bde
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-27 10:17:16 +0100

    add routes for auction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import CampaignDetails from './views/CampaignDetails';
 import CreateCampaign from './views/CreateCampaign';
 import Login from './views/Login';
 import Auctions from './views/Auctions';
+import BidInfoWrapper from './views/BidInfoWrapper';
+import BidInfo from './views/BidInfo';
 import AccountProvider from './context/account-context';
 import {
   BrowserRouter,
@@ -20,6 +22,10 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/auctions" element={<Auctions />}/>
+          <Route path="/auction" element={<BidInfoWrapper/>}>
+            <Route path=":itemId" element={<BidInfo />} />
+            <Route path="new" element={<CreateCampaign />} />
+          </Route>
           <Route path="/campaign" element={<CampaignWrapper/>}>
             <Route path=":campaignId" element={<CampaignDetails />} />
             <Route path="new" element={<CreateCampaign />} />
