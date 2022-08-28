f3d21a1de50f80a540ec1379c8154d04add30a46
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-28 14:08:32 +0100

    update links in header

--- frontend/src/views/Header.js
+++ frontend/src/views/Header.js
@@ -1,23 +1,27 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png'
+import { AccountContext } from '../context/account-context';
 
 function Header(props) {
   let navClass = 'navbar' ;
+  const { address } = useContext(AccountContext);
+  const addressStr = address.substring(0,10) + '...';
   
 
   return (
       <nav className={navClass}>
         <div className="container">
-          <Link to="/" id="logo" className="navbar-brand">
+          <Link to="/auctions" id="logo" className="navbar-brand">
             <img src={logo} alt={'logo'} width={120} />
           </Link>
         {
           props.showLinks === true && 
 
           <ul>
-            <li><Link to="/">Home</Link></li>
+            <li>Account: {addressStr}</li>
             <li>
-              <Link to="/campaign/new"><button type="button" className="btn btn-primary">Auction an item</button></Link>
+              <Link to="/auction/new"><button type="button" className="btn btn-primary">Auction an item</button></Link>
             </li>
           </ul>
 
