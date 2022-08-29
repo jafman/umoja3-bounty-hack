325fead80b027c6ec3b91722a049ecab1b2cfb3b
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-29 14:38:33 +0100

    update home page

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -2,43 +2,37 @@ import bidImage from '../assets/image_bid.png'
 import Header from './Header'
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { getCampaigns } from '../utils/db';
+import { getAuctions } from '../utils/db';
 import { loadStdlib } from '@reach-sh/stdlib';
 import { ALGO_MyAlgoConnect as MyAlgoConnect } from '@reach-sh/stdlib';
 
-const reach = loadStdlib('ALGO');
-reach.setWalletFallback(reach.walletFallback( { providerEnv: 'TestNet', MyAlgoConnect } ));
+// const reach = loadStdlib('ALGO');
+// reach.setWalletFallback(reach.walletFallback( { providerEnv: 'TestNet', MyAlgoConnect } ));
 
 function Home() {
-  const [campaigns, setCampaigns] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [ account, setAccount ] = useState({});
-  const [ address, setAddress ] = useState('');
+  // const [auctions, setAuctions] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [ account, setAccount ] = useState({});
+  // const [ address, setAddress ] = useState('');
 
-  const getAcc = async () => {
-    try {
-      const accountObj = await reach.getDefaultAccount();
-      console.log('ACCOUNT IS:', reach.formatAddress(accountObj))
-      setAccount(accountObj);
-      setAddress(reach.formatAddress(accountObj))
-    } catch (e) {
-      alert('Error getting account:', e)
-    }
+  // const getAcc = async () => {
+  //   try {
+  //     const accountObj = await reach.getDefaultAccount();
+  //     console.log('ACCOUNT IS:', reach.formatAddress(accountObj))
+  //     setAccount(accountObj);
+  //     setAddress(reach.formatAddress(accountObj))
+  //   } catch (e) {
+  //     alert('Error getting account:', e)
+  //   }
     
-  }
+  // }
 
-  const showAcc = () => {
-    console.log('ACCUNT:', reach.formatAddress(account));
-    alert('ACC ADD:' + address);
-  }
+  // const showAcc = () => {
+  //   console.log('ACCUNT:', reach.formatAddress(account));
+  //   alert('ACC ADD:' + address);
+  // }
 
-  useEffect(() => {
-    getCampaigns().then(campaigns => {
-      setCampaigns(campaigns);
-      //console.log(campaigns);
-      setLoading(false);
-    });
-  }, []);
+  
 
   return (
     <div className="landing">
