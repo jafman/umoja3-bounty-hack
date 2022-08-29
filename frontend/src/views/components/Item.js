107a7ffbc6bfaed7289f076ebcaa74aea914bf96
Jafar Kamarudeen <kamsy74@gmail.com>
2022-08-29 14:38:44 +0100

    update item properties

--- frontend/src/views/components/Item.js
+++ frontend/src/views/components/Item.js
@@ -1,18 +1,20 @@
 import algo from '../../assets/algo-logo.png';
-
+import { Link } from 'react-router-dom';
 function BidItem(props) {
   return (
     <div className="col-sm-4 campaign">
       <div className="card shadow-sm">
-        <img src={props.imgSrc} className="card-img-top" alt="..."/>
+        <img src={props.auction.imgUrl} className="card-img-top" alt="..."/>
         <div className="card-body bid-item">
-          <h5 className="card-title">Adidas Sneakers</h5>
+          <h5 className="card-title">{props.auction.title}</h5>
           <span className="card-text">Starting Bid</span>
           <div className='bid-price'>
-            <span className='price'>3</span>
+            <span className='price'>{props.auction.startingBid}</span>
             <img src={algo} width={15}/>
           </div>
-          <button className="btn btn-primary bid-item" disabled={!props.available}>Bid</button>
+          <Link to={`/auction/${props.auction.id}`}>
+            <button className="btn btn-primary bid-item" disabled={!props.available}>Bid</button>
+          </Link>
         </div>
       </div>
     </div>
